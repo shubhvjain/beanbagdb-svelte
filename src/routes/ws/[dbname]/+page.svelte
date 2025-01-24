d4b29e93e39f8ec9ac20b594e93f6b9374280def
shubh <shubhvjain@gmail.com>
2025-01-24 11:41:17 +0100

    testing custom ui

--- src/routes/ws/[dbname]/+page.svelte
+++ src/routes/ws/[dbname]/+page.svelte
@@ -5,13 +5,19 @@
 
   import Workspace from '$lib/ui/Workspace.svelte';
 
+	import Sample from '$lib/pages/Sample.svelte';
 
-
+let customUIPages = $state({
+	sample:{
+		component: Sample,
+		title: 'Sample Page'
+	}
+})
 
 </script>
 
 <!-- <h1>Workspace for database {data.name}</h1> -->
-<Workspace db={data} />
+<Workspace db={data} uiComponents={customUIPages} />
 
 <!-- {JSON.stringify(data,null,2)} -->
 
