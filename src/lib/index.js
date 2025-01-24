c4b69b6ab7871d3dc9feb953d511a02f2acdd669
shubh <shubhvjain@gmail.com>
2025-01-24 15:59:04 +0100

    new ver with imports exports

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,46 @@
 // Reexport your entry components here
+// Components from core
+export { default as Doc } from './core/Doc.svelte';
+export { default as EditDoc } from './core/EditDoc.svelte';
+export { default as NewDoc } from './core/NewDoc.svelte';
+export { default as SearchDocResults } from './core/SearchDocResults.svelte';
+export { default as SearchQuery } from './core/SearchQuery.svelte';
+export { default as SystemDoc } from './core/SystemDoc.svelte';
+
+// Components from db
+// No Svelte components to export from db
+
+// Components from pages
+export { default as DBPage } from './pages/DBPage.svelte';
+export { default as DBPlugins } from './pages/DBPlugins.svelte';
+export { default as DBSearch } from './pages/DBSearch.svelte';
+export { default as Document } from './pages/Document.svelte';
+export { default as Help } from './pages/Help.svelte';
+export { default as NewDocument } from './pages/NewDocument.svelte';
+export { default as Sample } from './pages/Sample.svelte';
+
+// Components from ui
+export { default as DBManager } from './ui/DBManager.svelte';
+export { default as WorkSpaceErrorPage } from './ui/WorkSpaceErrorPage.svelte';
+export { default as WorkSpaceHome } from './ui/WorkSpaceHome.svelte';
+export { default as Workspace } from './ui/Workspace.svelte';
+
+// Components from utils
+export { default as JSONEditor } from './utils/JSONEditor.svelte';
+export { default as LinkEditor } from './utils/LinkEditor.svelte';
+export { default as ObjectViewer } from './utils/ObjectViewer.svelte';
+export { default as SchemaEditor } from './utils/SchemaEditor.svelte';
+export { default as TableViewer } from './utils/TableViewer.svelte';
+export { default as TextEditor } from './utils/TextEditor.svelte';
+export { default as KeyEditor } from './utils/KeyEditor.svelte';
+export { default as LogViewer } from './utils/LogViewer.svelte';
+export { default as ObjectViewer2 } from './utils/ObjectViewer2.svelte';
+export { default as SettingEditor } from './utils/SettingEditor.svelte';
+export { default as TagsEditor } from './utils/TagsEditor.svelte';
+
+
+// Scripts from db
+export { default as beanbagdbweb } from './db/beanbagdbweb.js';
+export { default as pouchdbFind } from './db/pouchdb.find.js';
+export { default as pouchdbMin } from './db/pouchdb.min.js';
+export { default as textcommand } from './db/textcommand.js';
